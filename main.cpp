9b54ea6de680faf43f60c08191635ea8a2580542
J-d3v <A00840297@tec.mx>
2026-04-13 19:51:23 -0600

    todo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); // hace que cin y cout sean mas rapidos
+    cin.tie(nullptr); // evita que cout se vacie automaticamente antes de cada cin
+
+    cout << "--- Divide y Venceras --- \n";
+
+}
